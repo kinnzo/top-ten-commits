9cce3931f4ce1bf4c6632848eda91f3fba61cc00
ddmills <dmills@iastate.edu>
2015-09-09 12:42:16 -0500

    removed unused scripts and put everything in a window.load so fa icons show up

--- main.js
+++ main.js
@@ -1,31 +1,33 @@
 'use strict';
-let json = api.boa.run('top-ten-commits.boa');
-$('#loading').hide();
-$('#content').show();
-let count = 0;
-let labels = [];
-let dataset = [];
-for(let index in json.out["[]"]) {
-	count++;
-	let label =
-		$('#table-output-body').append(`<tr><td> ${count} </td> <td> ${index} </td> <td> ${json.out["[]"][index]} </td> </tr>`)
-		labels.push(index);
-	dataset.push(json.out["[]"][index]);
-}
-let chartData = {
-		labels: labels,
-		datasets: [{
-			fillColor: '#ff8080',
-			strokeColor: '#bf6060',
-			data: dataset
-		}]
-}
+$(window).load(function() {
+	let json = api.boa.run('top-ten-commits.boa');
+	$('#loading').hide();
+	$('#content').show();
+	let count = 0;
+	let labels = [];
+	let dataset = [];
+	for(let index in json.out["[]"]) {
+		count++;
+		let label =
+			$('#table-output-body').append(`<tr><td> ${count} </td> <td> ${index} </td> <td> ${json.out["[]"][index]} </td> </tr>`)
+			labels.push(index);
+		dataset.push(json.out["[]"][index]);
+	}
+	let chartData = {
+			labels: labels,
+			datasets: [{
+				fillColor: '#ff8080',
+				strokeColor: '#bf6060',
+				data: dataset
+			}]
+	}
 
-let canvas = document.createElement('canvas');
-canvas.setAttribute('width', '400px');
-canvas.setAttribute('height', '300px');
-canvas.id = "chart-output";
-$('#content').prepend(canvas);
+	let canvas = document.createElement('canvas');
+	canvas.setAttribute('width', '400px');
+	canvas.setAttribute('height', '300px');
+	canvas.id = "chart-output";
+	$('#content').prepend(canvas);
 
-let ctx = canvas.getContext('2d');
-new Chart(ctx).Bar(chartData, { 'responsive': true, });
+	let ctx = canvas.getContext('2d');
+	new Chart(ctx).Bar(chartData, { 'responsive': true, });
+});
